bfa02464110e16405bec7fb45ffc2a75f60495dc
David Adams <adams.david.10@gmail.com>
2018-07-30 22:36:47 -0500

    chore(versioning): add c2-semantic-release

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export * as actions from './actions'
+export * as actions from './actions' // eslint-disable-line
 export {default as formHOC} from './formHOC'
 export {default as reducer} from './reducer'
 export {default as form} from './selector'
