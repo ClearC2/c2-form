1c8cd2a63e9638866b1a099cd4187c10aeb17070
David Adams <adams.david.10@gmail.com>
2018-11-16 16:59:26 -0600

    feat(withForm): add reduxless withForm HOC

--- src/__tests__/withForm.test.js
+++ src/__tests__/withForm.test.js
@@ -1,12 +1,127 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
+import Component from '@reactions/component'
+import {render, fireEvent, cleanup} from 'react-testing-library'
 import withForm from '../withForm'
-import {render} from 'react-testing-library'
+
+function renderTest (sut) {
+  const result = render(sut)
+  return {
+    ...result,
+    getInitialValues: () => result.getByTestId('initial-values').innerHTML,
+    getCurrentValues: () => result.getByTestId('current-values').innerHTML,
+    clickButton: () => fireEvent.click(result.container.querySelector('button'))
+  }
+}
 
 describe('withForm', () => {
+  afterEach(cleanup)
+
   it('should render base component', () => {
-    const FooBar = () => <span>Foobar</span>
-    const WithFormFoobar = withForm(FooBar)
-    const {getByText} = render(<WithFormFoobar />)
+    const Test = () => <span>Foobar</span>
+    const WithFormFoobar = withForm(Test)
+    const {getByText} = renderTest(<WithFormFoobar />)
     getByText('Foobar')
+  })
+
+  it('should set initial and current values', () => {
+    const values = {foo: 'foo', bar: 'bar'}
+    const Test = ({initialValues, currentValues, setInitialValues}) => (
+      <Component didMount={() => setInitialValues(values)}>
+        <div data-testid='initial-values'>
+          {JSON.stringify(initialValues)}
+        </div>
+        <div data-testid='current-values'>
+          {JSON.stringify(currentValues)}
+        </div>
+      </Component>
+    )
+    const WithFormFoobar = withForm(Test)
+    const {getInitialValues, getCurrentValues} = renderTest(<WithFormFoobar />)
+    expect(getInitialValues()).toBe(JSON.stringify(values))
+    expect(getCurrentValues()).toBe(JSON.stringify(values))
+  })
+
+  it('should only set current value', () => {
+    const values = {foo: 'foo', bar: 'bar'}
+    const Test = ({initialValues, currentValues, setInitialValues, setValue}) => (
+      <Component didMount={() => setInitialValues(values)}>
+        <div data-testid='initial-values'>
+          {JSON.stringify(initialValues)}
+        </div>
+        <div data-testid='current-values'>
+          {JSON.stringify(currentValues)}
+        </div>
+        <button onClick={() => setValue('bar', 'BAR')}>
+          Click
+        </button>
+      </Component>
+    )
+    const WithFormFoobar = withForm(Test)
+    const {clickButton, getCurrentValues, getInitialValues} = renderTest(<WithFormFoobar />)
+    clickButton()
+    expect(getInitialValues()).toBe(JSON.stringify(values))
+    expect(getCurrentValues()).toBe(JSON.stringify({...values, bar: 'BAR'}))
+  })
+
+  it('should delete field', () => {
+    const values = {foo: 'foo', bar: 'bar'}
+    const Test = ({currentValues, setInitialValues, deleteField}) => (
+      <Component didMount={() => setInitialValues(values)}>
+        <div data-testid='current-values'>
+          {JSON.stringify(currentValues)}
+        </div>
+        <button onClick={() => deleteField('foo')}>
+          Click
+        </button>
+      </Component>
+    )
+    const WithFormFoobar = withForm(Test)
+    const {clickButton, getCurrentValues} = renderTest(<WithFormFoobar />)
+    clickButton()
+    expect(getCurrentValues()).toBe(JSON.stringify({bar: 'bar'}))
+  })
+
+  it('should pass isDirty flag', () => {
+    const values = {foo: 'foo', bar: 'bar'}
+    const Test = ({setInitialValues, setValue, isDirty}) => (
+      <Component didMount={() => setInitialValues(values)}>
+        <div>{isDirty ? 'DIRTY' : 'NOTDIRTY'}</div>
+        <button onClick={() => setValue('foo', 'FOO')}>
+          Click
+        </button>
+      </Component>
+    )
+    const WithFormFoobar = withForm(Test)
+    const {clickButton, getByText} = renderTest(<WithFormFoobar />)
+    getByText('NOTDIRTY')
+    clickButton()
+    getByText('DIRTY')
+  })
+
+  it('should reset', () => {
+    const values = {foo: 'foo', bar: 'bar'}
+    const Test = ({initialValues, currentValues, setInitialValues, setValue, reset}) => (
+      <Component didMount={() => setInitialValues(values)}>
+        <div data-testid='initial-values'>
+          {JSON.stringify(initialValues)}
+        </div>
+        <div data-testid='current-values'>
+          {JSON.stringify(currentValues)}
+        </div>
+        <button data-testid='set-value' onClick={() => setValue('foo', 'FOO')}>
+          set
+        </button>
+        <button data-testid='reset' onClick={reset}>
+          reset
+        </button>
+      </Component>
+    )
+    const WithFormFoobar = withForm(Test)
+    const {getByTestId, getCurrentValues, getInitialValues} = renderTest(<WithFormFoobar />)
+    fireEvent.click(getByTestId('set-value'))
+    expect(getCurrentValues()).not.toBe(getInitialValues())
+    fireEvent.click(getByTestId('reset'))
+    expect(getCurrentValues()).toBe(getInitialValues())
   })
 })
--- src/withForm.js
+++ src/withForm.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Map} from 'immutable'
+import {Map, fromJS} from 'immutable'
 
 const withForm = (BaseComponent) => {
   return class extends Component {
@@ -7,12 +7,39 @@ const withForm = (BaseComponent) => {
       initialValues: Map(),
       currentValues: Map()
     }
+    setInitialValues = (values) => {
+      values = fromJS(values)
+      this.setState({
+        initialValues: values,
+        currentValues: values
+      })
+    }
+    setValue = (field, value) => {
+      this.setState({
+        currentValues: this.state.currentValues.set(field, fromJS(value))
+      })
+    }
+    deleteField = (field) => {
+      this.setState({
+        currentValues: this.state.currentValues.delete(field)
+      })
+    }
+    reset = () => {
+      this.setState({
+        currentValues: this.state.initialValues
+      })
+    }
     render () {
       const {initialValues, currentValues} = this.state
       return (
         <BaseComponent
           {...this.props}
-          isDirty={initialValues.equals(currentValues)}
+          {...this.state}
+          setInitialValues={this.setInitialValues}
+          setValue={this.setValue}
+          deleteField={this.deleteField}
+          reset={this.reset}
+          isDirty={!initialValues.equals(currentValues)}
         />
       )
     }
